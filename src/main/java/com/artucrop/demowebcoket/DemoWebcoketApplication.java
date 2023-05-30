e50ac68d68bf2e9b599046a92aa8b64a68d40331
abmaschietto <arturboudoux@gmail.com>
2023-05-30 19:58:24 -0300

    initial config

---
+++ src/main/java/com/artucrop/demowebcoket/DemoWebcoketApplication.java
@@ -0,0 +1,13 @@
+package com.artucrop.demowebcoket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebcoketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebcoketApplication.class, args);
+	}
+
+}
